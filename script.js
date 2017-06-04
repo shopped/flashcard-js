3bf12222af2453e507aed24d0ac8c99a03726627
Patrick Carroll <Patrick Carroll>
2017-06-04 12:41:07 -0700

    Added parsing functionality and flashcard navigation buttons

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function generate()
 	var entrytext = document.getElementById("entry-text");
 	var button = document.getElementById("entry-text");
 	var bump = document.getElementById("bump-down");
+	var text = document.getElementById("text");
 	cardcontainer.style.display = "block";
 	entrycontainer.style.display = "none";
 	entrytext.innerHTML="Edit Cards";
 	bump.style.display = "block";
-	parse();
+	parse(text.value);
 	entrytext.onclick=function(){regenerate();};
 }
 function regenerate()
@@ -27,7 +28,36 @@ function regenerate()
 	bump.style.display = "none";
 	entrytext.onclick=function(){generate();};
 }
-function parse()
+function parse(arg)
 {
-	console.log('parse');
+	var lines = arg.split("\n");
+	var front = [];
+	var back = [];
+	var num_cards = 0;
+	for (i = 0; i < lines.length; i++)
+	{
+		front.push(lines[i].split(":")[0].trim());
+		back.push(lines[i].split(":")[1].trim());
+		num_cards++;
+	}
+}
+
+function next()
+{
+	console.log('next')
+}
+
+function flip()
+{
+	console.log('flip')
+}
+
+function previous()
+{
+	console.log('previous')
+}
+
+function random()
+{
+	console.log('random')
 }
