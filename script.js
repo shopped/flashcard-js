1006db865a08d6b2d6755131a2b9aaf12aeaecca
Patrick Carroll <Patrick Carroll>
2017-06-04 12:26:08 -0700

    Button swaps between text input and card display

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+cards = {};
+function generate()
+{
+	var cardcontainer = document.getElementById("card-container");
+	var entrycontainer = document.getElementById("entry-container");
+	var entrytext = document.getElementById("entry-text");
+	var button = document.getElementById("entry-text");
+	var bump = document.getElementById("bump-down");
+	cardcontainer.style.display = "block";
+	entrycontainer.style.display = "none";
+	entrytext.innerHTML="Edit Cards";
+	bump.style.display = "block";
+	parse();
+	entrytext.onclick=function(){regenerate();};
+}
+function regenerate()
+{
+	var cardcontainer = document.getElementById("card-container");
+	var entrycontainer = document.getElementById("entry-container");
+	var entrytext = document.getElementById("entry-text");
+	var button = document.getElementById("entry-text");
+	var bump = document.getElementById("bump-down");
+	cardcontainer.style.display = "none";
+	entrycontainer.style.display = "block";
+	entrytext.innerHTML="Generate";
+	bump.style.display = "none";
+	entrytext.onclick=function(){generate();};
+}
+function parse()
+{
+	console.log('parse');
+}
