7e17d7d47204296e0461bac16bbc75e4ec4dfc21
Patrick Carroll <patrickgesarcarroll@gmail.com>
2017-07-29 15:23:23 -0700

    Images for chapter 6 show, and next functionality works

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var back=[];
 var num_cards=0;
 var current_card=0;
 var flipped=false;
-var cards = {};
+var cards = [];
 
 var loc;
 var dir;
@@ -20,7 +20,7 @@ function new_deck(){
 	header.style.display = "none";
 	body.style.display = "none";
 }
-
+	
 
 function generate_deck(){
 	loc = window.location.pathname;
@@ -41,21 +41,22 @@ function generate_deck(){
 	var label = document.getElementById("header-label");
 	label.innerHTML = "Chapters: ";
 	for (var i=0; i<4; i++){
-		if (document.getElementById(chapters[i].toString()).checked)
 		{
-			//parse_images_and_make_dictionary(chapters[i].toString())
+			parse_images_and_make_dictionary(chapters[i].toString())
 			if (!first)
 				label.innerHTML += ", "
 			label.innerHTML += chapters[i].toString();
 			first = false;
 		}
 	}
-
+	current_card = -1;
+	next();
 }
 
 //cards array will hold all of these
 function parse_images_and_make_dictionary(chapter)
 {
+	num_cards = 0;
 	//starts a loop from 1 to 1000
 	for (var i=1;i<25;i++) //$$$FIX THIS
 	{
@@ -68,21 +69,32 @@ function parse_images_and_make_dictionary(chapter)
 
 		var card = {};
 		card.chapter = chapter;
-		card.front = "";
-		card.back = "";
+		card.front = dir+"/$/$".replace("$", chapter).replace("$", chapter)+".$.png".replace("$", i);
+		card.back = dir+"/$/$".replace("$", chapter).replace("$", chapter)+".$b.png".replace("$", i);
 		//generates a front and back and saves in object
-		if (true){ //hint exists
+		if (false){ //hint exists
 			//store hint
 			card.hint = true;
 		} else {
 			card.hint = false;
 		}
 		cards.push(card); //adds that object to the cards array
+		num_cards++;
 	}
 	
 }
+//var num_cards
+//var current_card
+//array cards
 function next(){
-
+	current_card++;
+	if (current_card == num_cards - 1)
+		current_card = 0
+	var card = cards[current_card];
+	console.log(card);
+	document.getElementById('front_img').src = card.front;
+	document.getElementById('back_img').src = card.back;
+	//document.getElementById('hint_img').src = dir +'/6/6.1.png';
 }
 function correct(){
 	//Will remove the card
