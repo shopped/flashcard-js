a1f372175d70bcdcb90a038b02d64b002a30b5a8
Patrick Carroll <patrickgesarcarroll@gmail.com>
2017-07-29 15:05:54 -0700

    Images can be loaded now

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var current_card=0;
 var flipped=false;
 var cards = {};
 
+var loc;
+var dir;
+
 function new_deck(){
 	//Show Selection
 	var a = document.getElementById("chapter-selection");
@@ -18,7 +21,10 @@ function new_deck(){
 	body.style.display = "none";
 }
 
+
 function generate_deck(){
+	loc = window.location.pathname;
+	dir = loc.substring(0, loc.lastIndexOf('/'));
 	//Hide Selection
 	var a = document.getElementById("chapter-selection");
 	var b = document.getElementById("generate-button");
@@ -29,7 +35,7 @@ function generate_deck(){
 	var body= document.getElementById("deck-body");
 	header.style.display = "block";
 	body.style.display = "block";
-
+	document.getElementById('front_img').src = dir +'/6/6.1.png';
 	var chapters = [6, 7, 8, 10];
 	var first = true;
 	var label = document.getElementById("header-label");
@@ -37,12 +43,43 @@ function generate_deck(){
 	for (var i=0; i<4; i++){
 		if (document.getElementById(chapters[i].toString()).checked)
 		{
+			//parse_images_and_make_dictionary(chapters[i].toString())
 			if (!first)
 				label.innerHTML += ", "
 			label.innerHTML += chapters[i].toString();
 			first = false;
 		}
 	}
+
+}
+
+//cards array will hold all of these
+function parse_images_and_make_dictionary(chapter)
+{
+	//starts a loop from 1 to 1000
+	for (var i=1;i<25;i++) //$$$FIX THIS
+	{
+		//checks that a card
+		//try{
+		//	open();//cwd/chapter/chapter.i.png
+		//} catch {
+		//	return;
+		//}
+
+		var card = {};
+		card.chapter = chapter;
+		card.front = "";
+		card.back = "";
+		//generates a front and back and saves in object
+		if (true){ //hint exists
+			//store hint
+			card.hint = true;
+		} else {
+			card.hint = false;
+		}
+		cards.push(card); //adds that object to the cards array
+	}
+	
 }
 function next(){
 
