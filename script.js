ee5a81530a9672e8e25613e7ee405a6197429aa1
Patrick Carroll <patrickgesarcarroll@gmail.com>
2017-07-30 21:18:45 -0700

    The deck loops based on a dictionary of number of cards in a chapter

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var front=[];
 var back=[];
 var num_cards=0;
+var right, wrong;
 var current_card=0;
 var flipped=false;
 var cards = [];
+var cards_per_chapter = {'6':26, '7':0, '8':0, '10':0, '11':0};
 
 var loc;
 var dir;
@@ -42,6 +44,7 @@ function generate_deck(){
 	var first = true;
 	var label = document.getElementById("header-label");
 	label.innerHTML = "Chapters: ";
+	num_cards = 0;
 	for (var i=0; i<4; i++){
 		{
 			parse_images_and_make_dictionary(chapters[i].toString())
@@ -52,15 +55,15 @@ function generate_deck(){
 		}
 	}
 	current_card = -1;
+	right = wrong = 0;
 	next();
 }
 
 //cards array will hold all of these
 function parse_images_and_make_dictionary(chapter)
 {
-	num_cards = 0;
-	//starts a loop from 1 to 1000
-	for (var i=1;i<25;i++) //$$$FIX THIS
+	//starts a loop
+	for (var i=1;i<cards_per_chapter[chapter]+1;i++)
 	{
 		var card = {};
 		card.chapter = chapter;
@@ -78,6 +81,8 @@ function parse_images_and_make_dictionary(chapter)
 //var current_card
 //array cards
 function next(){
+	console.log(cards);
+	console.log(num_cards);
 	current_card++;
 	if (current_card == num_cards - 1)
 		current_card = 0
@@ -87,24 +92,30 @@ function next(){
 	document.getElementById('back_img').src = card.back;
 	document.getElementById("back_img").style.display = "none";
 	//generate hint
-		document.getElementById('hint-btn').onclick=hint;
-		document.getElementById('hint_img').src = card.hint;
-		document.getElementById('hint-btn').style.backgroundColor = 'steelblue';
+	document.getElementById('hint-btn').onclick=hint;
+	document.getElementById('hint_img').src = card.hint;
+	document.getElementById('hint-btn').style.backgroundColor = 'steelblue';
+	document.getElementById('hint-btn').style.color = 'white';
 	//will throw nohint on error
 	document.getElementById('hint_img').style.display = "none";
-	//document.getElementById('hint_img').src = dir +'/6/6.1.png';
 }
 function nohint(){
 	document.getElementById('hint-btn').onclick="";
-		document.getElementById('hint_img').src = '';
-		document.getElementById('hint-btn').style.backgroundColor = 'gray';
+	document.getElementById('hint_img').src = '';
+	document.getElementById('hint-btn').style.backgroundColor = 'gray';
+	document.getElementById('hint-btn').style.color = 'black';
+
 }
 function correct(){
+	next();
+	right++;
 	//Will remove the card
 	//Will add one to correct
 	//Will call next function
 }
-function wrong(){
+function missed(){
+	next();
+	wrong++;
 	//Will place the missed card at the end of the deck
 	//Will add one to missed
 	//Will call next function
